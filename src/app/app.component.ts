8fae9e3b270171262b9143785e1b500ce3982bc4
Alberto Denti <albertodenti2@gmail.com>
2024-01-05 18:05:19 +0100

    refactor: migrated board component to four-in-a-row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,14 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BoardComponent } from '../components/board/board.component';
-import { ModalService } from './core/service/modal.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BoardComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  @ViewChild('test', { static: true }) test: TemplateRef<HTMLElement> | null =
-    null;
   title = '4-in-a-row';
-  constructor(private modalService: ModalService) {}
-  ngOnInit() {
-    if (this.test) {
-      const modal = this.modalService.openModal(this.test, {
-        title: 'test',
-        size: 'lg',
-        position: 'center',
-        showCloseButton: true,
-        backdropDismiss: false,
-      });
-
-      modal.closeEvent.subscribe({
-        next: () => {
-          // alert('closed');
-        },
-      });
-    }
-  }
 }
