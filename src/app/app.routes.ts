f25b9db2690be904a2e401ff5fa2d6ecb0256de4
Alberto Denti <albertodenti2@gmail.com>
2024-01-05 13:12:00 +0100

    feat: rooms list route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'rooms',
+  },
+  {
+    path: 'rooms',
+    loadComponent: () =>
+      import('./pages/room-list/room-list.component').then(
+        (m) => m.RoomListComponent
+      ),
+  },
+];
