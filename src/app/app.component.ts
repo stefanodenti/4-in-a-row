d74808569907773dcdc7e2ae15cfb959458658d6
Stefano <stefanodenti4@gmail.com>
2024-01-05 12:56:17 +0100

    feat: addModal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BoardComponent } from '../components/board/board.component';
+import { ModalService } from './core/service/modal.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, BoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  @ViewChild('test', { static: true }) test: TemplateRef<HTMLElement> | null =
+    null;
   title = '4-in-a-row';
+  constructor(private modalService: ModalService) {}
+  ngOnInit() {
+    if (this.test) {
+      const modal = this.modalService.openModal(this.test, {
+        title: 'test',
+        size: 'lg',
+        position: 'center',
+        showCloseButton: true,
+        backdropDismiss: false,
+      });
+      modal.closeEvent.subscribe({
+        next: () => {
+          alert('closed')
+        },
+      });
+    }
+  }
 }
---
+++ src/app/core/service/modal.service.ts
@@ -0,0 +1,57 @@
+import {
+  ApplicationRef, ComponentRef, createComponent,
+  Inject,
+  Injectable,
+  TemplateRef,
+  Type
+} from '@angular/core';
+import {ModalComponent} from "../components/modal/modal.component";
+import {Subject, take} from "rxjs";
+import {DOCUMENT} from "@angular/common";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  constructor(
+    private applicationRef: ApplicationRef,
+    @Inject(DOCUMENT) private document: Document) {
+  }
+
+  openModal<T>(content: TemplateRef<any> | Type<T>, options?: {
+    title?: string,
+    showCloseButton?: boolean,
+    backdropDismiss?: boolean,
+    position?: 'center' | 'top' | 'bottom',
+    size?: 'sm' | 'md' | 'lg'
+  }) {
+    let modalComponent: ComponentRef<ModalComponent>;
+    if (content instanceof TemplateRef) {
+      const embeddedView = content.createEmbeddedView(null);
+      modalComponent = createComponent(ModalComponent,
+        {
+          environmentInjector: this.applicationRef.injector,
+          projectableNodes: [embeddedView.rootNodes]
+        });
+      this.applicationRef.attachView(embeddedView);
+    } else {
+      modalComponent = createComponent(ModalComponent, {environmentInjector: this.applicationRef.injector});
+      const componentViewRef = modalComponent.instance.modalDirective.viewContainerRef;
+      componentViewRef.clear();
+      const component = componentViewRef.createComponent(content);
+      component.hostView.detectChanges();
+    }
+    modalComponent.instance.title = options?.title;
+    modalComponent.instance.showCloseButton = options?.showCloseButton ?? true;
+    modalComponent.instance.backdropDismiss = options?.backdropDismiss ?? false;
+    modalComponent.instance.position = options?.position ?? 'center';
+    modalComponent.instance.size = options?.size ?? 'lg';
+    modalComponent.hostView.detectChanges();
+    modalComponent.changeDetectorRef.detectChanges();
+    modalComponent.instance.closeEvent.pipe(take(1)).subscribe({
+      next: () => modalComponent.destroy(),
+    })
+    this.document.body.appendChild(modalComponent.location.nativeElement);
+    return modalComponent.instance;
+  }
+}
